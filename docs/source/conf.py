f8df5eaecca3d07f28f5995326930d9ff5e9a8bc
billma007 <maboning237103015@163.com>
2022-04-11 12:07:46 +0800

    1.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,6 @@ release = '1.4.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-     'recommonmark',
-     'sphinx_markdown_tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
